ecdc6ca35915e5cd3e2e11c1459ec53c599a534c
Fabrice Brito <fabrice.brito@terradue.com>
2014-05-29 09:06:30 +0200

    Update opensearch.R

--- src/main/R/rOpenSearch/R/opensearch.R
+++ src/main/R/rOpenSearch/R/opensearch.R
@@ -1,11 +1,11 @@
 
 
-opensearch <- function(osd, count=20, ...) {
+opensearch <- function(osd, count=20, start.page=0, start.index, .params) {
 
 
 }
 
-get.osd.template <- function(URL, format="RDF") {
+get.osd.template <- function(URL, format="application/rdf+xml") {
 
 
 }
