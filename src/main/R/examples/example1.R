0d8a4b697eb661c993d169a7b5e14a412669890f
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-03 13:34:07 +0200

    Update example1.R

--- src/main/R/examples/example1.R
+++ src/main/R/examples/example1.R
@@ -9,7 +9,11 @@ value <- c(100, "2010-01-10", "2010-01-31")
 type <- c("count", "time:start", "time:end")
 df.params <- data.frame(type, value)
 
-res <- Query(osd.url,df.params)
+res <- Query(osd.url, "application/rdf+xml", df.params)
+
+dataset <- xmlToDataFrame(nodes = getNodeSet(res, 
+    "//dclite4g:DataSet"), stringsAsFactors = FALSE)
+  
 
 # create a SpatialPolygonsDataFrame with the first element of res$dataset
 poly.sp <- SpatialPolygonsDataFrame(readWKT(data.frame(res$dataset$spatial)[1,]), res$dataset[1,])
