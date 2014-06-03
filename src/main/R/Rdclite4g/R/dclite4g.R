c414de1c455d03c406c61757f3ccb0e271da9b57
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-03 12:18:24 +0200

    Update dclite4g.R

--- src/main/R/Rdclite4g/R/dclite4g.R
+++ src/main/R/Rdclite4g/R/dclite4g.R
@@ -62,7 +62,7 @@ GetOSQueriables <- function(opensearch.description) {
   df.full.template <- data.frame(matrix(unlist(l), nrow=length(l), byrow=T), stringsAsFactors=FALSE)
 
   # keep the column with the queryables' type and add a value column with NAs
-  df.template <- df.full.template[,2]
+  df.template <- data.frame(df.full.template[,2], stringsAsFactors=FALSE)
   df.template[, 2] <- NA
 
   # set the column names to type/value, it will be very useful for the Query function params argument 
