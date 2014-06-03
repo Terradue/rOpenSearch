72a1c6abd6442ce9b68edd169ae951a97ba98083
Fabrice Brito <fabrice.brito@terradue.com>
2014-06-03 14:20:21 +0200

    Update example1.R

--- src/main/R/examples/example1.R
+++ src/main/R/examples/example1.R
@@ -1,31 +1,36 @@
 library(devtools)
+library(rgdal)
+library(rgeos)
+
 install_github("Rdclite4g", username="Terradue", subdir="/src/main/R/Rdclite4g")
+library(Rdclite4g)
 
 # the OpenSearch description document
 osd.url <- "http://eo-virtual-archive4.esa.int/search/ASA_IM__0P/description"
 
-# create filter
-value <- c(100, "2010-01-10", "2010-01-31")
-type <- c("count", "time:start", "time:end")
-df.params <- data.frame(type, value)
+# get the queryables dataframe from the OpenSearch description URL
+df.params <- GetOSQueriables(osd.url)
 
+# define the values for the queryables
+df.params$value[df.params$type == "count"] <- 30 
+df.params$value[df.params$type == "time:start"] <- "2010-01-10"
+df.params$value[df.params$type == "time:end"] <- "2010-01-31"
+
+# submit the query
 res <- Query(osd.url, "application/rdf+xml", df.params)
 
+# get the dataset
 dataset <- xmlToDataFrame(nodes = getNodeSet(res, 
-    "//dclite4g:DataSet"), stringsAsFactors = FALSE)
-  
+  "//dclite4g:DataSet"), stringsAsFactors = FALSE)
 
 # create a SpatialPolygonsDataFrame with the first element of res$dataset
-poly.sp <- SpatialPolygonsDataFrame(readWKT(data.frame(res$dataset$spatial)[1,]), res$dataset[1,])
+poly.sp <- SpatialPolygonsDataFrame(readWKT(data.frame(dataset$spatial)[1,]), dataset[1,])
 
 # iterate through the remaining dataset
-for (n in 2:nrow(res$dataset)) {
+for (n in 2:nrow(dataset)) {
   poly.sp <- rbind(poly.sp,
-    SpatialPolygonsDataFrame(readWKT(data.frame(res$dataset$spatial)[n,],id=n), res$dataset[n,]))
+    SpatialPolygonsDataFrame(readWKT(data.frame(dataset$spatial)[n,],id=n), dataset[n,]))
 }
-
-# load rgdal for OGR
-library(rgdal)
 
 # write the geojson file
 writeOGR(poly.sp, 'example1.geojson','dataMap', driver='GeoJSON')
